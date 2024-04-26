e9bd14b3b270c8437a08eb4f42caeb93e5630d7a
OlehOrenchuk <oleh.orenchuk@gmail.com>
2024-04-26 12:42:54 +0300

    role functionallity added ,rights limitation added (superAdmin/viewer)

--- src/hocs/withAuth.tsx
+++ src/hocs/withAuth.tsx
@@ -7,6 +7,8 @@ const withAuth = <P extends object>(Component: ComponentType<P>) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(
       null
     );
+    const [userType, setUserType] = useState<string | null>(null);
+
     const router = useRouter();
 
     useEffect(() => {
@@ -14,6 +16,7 @@ const withAuth = <P extends object>(Component: ComponentType<P>) => {
         try {
           const response = await auth.get("/me");
           setIsAuthenticated(response.status === 200);
+          setUserType(response.data?.userType);
         } catch (error) {
           setIsAuthenticated(false);
           console.error("Error fetching user data:", error);
@@ -28,6 +31,17 @@ const withAuth = <P extends object>(Component: ComponentType<P>) => {
         router.replace("auth/login");
       }
     }, [isAuthenticated, router]);
+
+    // Redirect to home if userType is 'viewer'
+    useEffect(() => {
+      if (
+        isAuthenticated &&
+        userType === "viewer" &&
+        router.pathname === "/admin"
+      ) {
+        router.replace("/");
+      }
+    }, [isAuthenticated, userType, router]);
 
     // Render the component if authenticated
     return isAuthenticated ? <Component {...(props as P)} /> : null;
